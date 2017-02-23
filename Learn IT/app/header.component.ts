c746ccce1a8157bfd456242f0e091c90c534c4c9
andumy <andrei_dumitrescu_96@yahoo.com>
2017-02-23 11:20:52 +0200

    Initial Commit
    23-02-2017
    -------------------------------------
    Navbar & SideMenu reworked for Angular 2

---
+++ Learn IT/app/header.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+    selector: 'hdr',
+    templateUrl: 'app/header.component.html',
+    styleUrls: ['app/header.component.css']
+})
+
+export class HeaderComponent {
+
+}
---
+++ Learn IT/app/sideMenu.component.ts
@@ -0,0 +1,12 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+    selector: 'sidemn',
+    templateUrl: 'app/sideMenu.component.html',
+    styleUrls: ['app/sideMenu.component.css']
+})
+
+export class SidemenuComponent {
+
+}
