2b049432e206102e46bc7700283298198919c06b
Andru Mocanu <andru.mao94@gmail.com>
2017-03-02 21:53:45 +0200

    compile works

--- backend/LearnIT/CoreApp/views.py
+++ backend/LearnIT/CoreApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, HttpRequest
-import subprocess
+from django.views.decorators.csrf import csrf_exempt
+import subprocess, json
 
 
 # Create your views here.
@@ -8,6 +9,7 @@ def index(request):
     return HttpResponse('HelloWorld')
 
 
+@csrf_exempt
 def run(request: HttpRequest):
     """
     :param request: http request for this route
@@ -15,23 +17,37 @@ def run(request: HttpRequest):
     :return: json response for this route
     :rtype: JsonResponse
     """
-    fin = open('main.c', 'w')
-    fin.write('#include<stdio.h>\n\nint main() {\n\tprintf("Hello world!\\n");\n\treturn 0\n}')
-    fin.close()
-    compile_process = subprocess.Popen(['gcc', 'main.c', '-Wall'])
-    output, error = compile_process.communicate()
-    compile_process.wait()
-    if error is not None:
+    if request.method == 'POST':
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        code = body['code']
+
+        fin = open('main.c', 'w')
+        fin.write(code)
+        fin.close()
+
+        compile_process = subprocess.Popen(['gcc', 'main.c', '-Wall'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error = compile_process.communicate()
+        compile_process.wait()
+
+        if error is not None:
+            response = {
+                'error': True,
+                'output': error.decode("utf-8")
+            }
+        else:
+            run_process = subprocess.Popen(['./a.out'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            output, error = run_process.communicate()
+            response = {
+                'error': False if error is None else True,
+                'output': output.decode("utf-8")
+            }
+
+        clean_process = subprocess.Popen(['rm', '-rf', 'main.c', 'a.out'])
+    else:
         response = {
             'error': True,
-            'output': error
+            'output': 'GET'
         }
-    else:
-        run_process = subprocess.Popen(['./a.out'], stdout=subprocess.PIPE)
-        output, error = run_process.communicate()
-        response = {
-            'error': False if error is None else True,
-            'output': output.decode("utf-8")
-        }
-    clean_process = subprocess.Popen(['rm', '-rf', 'main.c', 'a.out'])
+
     return JsonResponse(response)
